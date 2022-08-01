ff428d3e8c5bc7caf86a40d71fd9fcb8252ad7b1
arriann <arriann.lee@gmail.com>
2022-08-01 14:34:12 +0100

    Create a function that displays none when clicking on an element

---
+++ bachelor-code/1/js/main.js
@@ -0,0 +1,10 @@
+document.querySelector('#finalRose').addEventListener('click', hide)
+
+function hide(){
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector('#sharleen').style.display = 'none'
+}
+
+
+// nikki wins
+// click on final rose
